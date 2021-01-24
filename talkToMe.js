46772eca59eb389cdd7893bb44fd4b59297907d4
Nick Codeacademy Pal <pozdnyakovnikolay@gmail.com>
2021-01-25 00:50:40 +0300

    ready programm

---
+++ talkToMe.js
@@ -0,0 +1,13 @@
+const Mess = {
+    firstone: ['Hey,you!', 'Hey there!','Hi!','Hello,','Yo,','Aye!'],
+    secondone: ['Nice to meet!','Glad to see ya,', 'Go away, bastard,'],
+    thirdone: ['Russia without Putin!', 'let us save the whales', 'happiness is not here']
+}
+
+//console.log(Mess);
+//console.log('Heee');
+//console.log('Heffee');
+//console.log(Mess.firstone.length);
+//console.log(Math.floor(Math.random() * Mess.firstone.length));
+
+console.log(Mess.firstone[Math.floor(Math.random() * Mess.firstone.length)] + ' ' + Mess.secondone[Math.floor(Math.random() * Mess.secondone.length)] + ' ' + Mess.thirdone[Math.floor(Math.random() * Mess.thirdone.length)])
